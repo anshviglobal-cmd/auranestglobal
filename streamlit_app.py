3976f0a343a7e97c9d8dbdf7ecf63d77da4ba726
anshviglobal-cmd <anshviglobal@gmail.com>
2026-01-18 14:03:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,13 @@
+import streamlit as st
+from pathlib import Path
 
+st.set_page_config(
+    page_title="AuraNest Global Solutions",
+    layout="wide"
+)
+
+# Load HTML
+html_file = Path("index.html").read_text(encoding="utf-8")
+
+# Render HTML
+st.components.v1.html(html_file, height=2000, scrolling=True)
