77dc79bb8aac534ddc48a70970d5ab61d36daedc
anshviglobal-cmd <anshviglobal@gmail.com>
2026-01-18 14:26:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,27 @@
 import streamlit as st
 from pathlib import Path
+import base64
 
 st.set_page_config(
     page_title="AuraNest Global Solutions",
     layout="wide"
 )
 
-# Load HTML
-html_file = Path("index.html").read_text(encoding="utf-8")
+def load_image(image_path):
+    return base64.b64encode(
+        Path(image_path).read_bytes()
+    ).decode()
 
-# Render HTML
-st.components.v1.html(html_file, height=2000, scrolling=True)
+# Load images
+EFuseConnect = load_image("assets/im1.jpeg")
+Eutility = load_image("assets/Eutility.jpg")
+
+# Load HTML
+html = Path("index.html").read_text(encoding="utf-8")
+
+# Inject images into HTML
+html = html.replace("{{EFuseConnect}}", EFuseConnect)
+html = html.replace("{{Eutility}}", Eutility)
+
+# Render
+st.components.v1.html(html, height=2200, scrolling=True)
