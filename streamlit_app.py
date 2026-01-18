e506e90575a5e83cd9a80d0c450926d7569b15e4
anshviglobal-cmd <anshviglobal@gmail.com>
2026-01-18 14:39:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,13 @@ def load_image(image_path):
 # Load images
 EFuseConnect = load_image("assets/im1.jpeg")
 Eutility = load_image("assets/Eutility.jpg")
+BuildOps360 = load_image("assets/BuildOps360.png")
+COMSID1 = load_image("assets/2.jpg")
+COMSID2 = load_image("assets/3.png")
+CloudFEAT = load_image("assets/CloudFEAT.jpg")
+MarinPRO = load_image("assets/MarinPRO.jpg")
+Education = load_image("assets/Education.png")
+
 
 # Load HTML
 html = Path("index.html").read_text(encoding="utf-8")
@@ -22,6 +29,12 @@ html = Path("index.html").read_text(encoding="utf-8")
 # Inject images into HTML
 html = html.replace("{{EFuseConnect}}", EFuseConnect)
 html = html.replace("{{Eutility}}", Eutility)
+html = html.replace("{{BuildOps360}}", BuildOps360)
+html = html.replace("{{COMSID1}}", COMSID1)
+html = html.replace("{{COMSID2}}", COMSID2)
+html = html.replace("{{CloudFEAT}}", CloudFEAT)
+html = html.replace("{{MarinPRO}}", MarinPRO)
+html = html.replace("{{Education}}", Education)
 
 # Render
 st.components.v1.html(html, height=2200, scrolling=True)
